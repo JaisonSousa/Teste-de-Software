b2b35e662441bb4983d6b80aba09b7cf13133815
Jaison de Sousa <jaisonangel@hotmail.com>
2021-12-08 00:41:21 -0300

    Nova atualização no projeto rest

--- Projetos_Automatizados/CursoRest/src/main/java/br/ce/wcjaison/rest/OlaMundoTest.java
+++ Projetos_Automatizados/CursoRest/src/main/java/br/ce/wcjaison/rest/OlaMundoTest.java
@@ -1,20 +1,32 @@
 package br.ce.wcjaison.rest;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
-public class OlaMundo {
+public class OlaMundoTest {
 	
-	public static void main(String[] args) {
-		//Atalho Ctrl + 1 cria variavel
+	@Test
+	public void testOlaMundo() {
+		
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
 		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
 		System.out.println(response.getStatusCode() == 200);
 		
+		 Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		 Assert.assertTrue(response.getStatusCode() == 200);
+		 Assert.assertTrue("O status code deveria ser 200" ,response.getStatusCode() == 200);
+		 Assert.assertEquals(200, response.statusCode());
+		 
+		
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
+		
+
 	}
 
 }
