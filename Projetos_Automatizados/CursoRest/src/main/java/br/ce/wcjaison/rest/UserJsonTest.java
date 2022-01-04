a4e1007ae62f0ac977793addfdfcf3de987addfa
Jaison de Sousa <jaisonangel@hotmail.com>
2022-01-04 00:23:13 -0300

    nova atualização de exemplo

--- Projetos_Automatizados/CursoRest/src/main/java/br/ce/wcjaison/rest/UserJsonTest.java
+++ Projetos_Automatizados/CursoRest/src/main/java/br/ce/wcjaison/rest/UserJsonTest.java
@@ -2,6 +2,9 @@ package br.ce.wcjaison.rest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
+
+import java.util.Arrays;
+
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Assert;
@@ -47,6 +50,101 @@ public class UserJsonTest {
 		
 
 	}
+	
+	@Test
+	public void deveVerificarSegundoNivel() {
+		
+		given() // Pré Condições
+		.when()
+		.get("https://restapi.wcaquino.me/users/2") // Ação
+		.then() // Assertivas
+		.statusCode(200)
+		.body("name", containsString("Joaquina"))
+		.body("endereco.rua", is("Rua dos bobos"));
+		
+
+	}
+	
+	@Test
+	public void deveVerificarLista() {
+		
+		given() // Pré Condições
+		.when()
+		.get("https://restapi.wcaquino.me/users/3") // Ação
+		.then() // Assertivas
+		.statusCode(200)
+		.body("name", containsString("Ana"))
+		.body("filhos[0].name", is("Zezinho"))
+		.body("filhos[1].name", is("Luizinho"))
+		.body("filhos.name", hasItem("Zezinho"))
+		.body("filhos.name", hasItems("Zezinho", "Luizinho"))
+		;
+		
+	}
+	
+	@Test
+	public void deveRetornarErroUsuarioInesixistente() {
+		
+		given() // Pré Condições
+		.when()
+		.get("https://restapi.wcaquino.me/users/4") // Ação
+		.then() // Assertivas
+		.statusCode(404)
+		.body("error", is("Usuário inexistente"))
+		;
+		
+	}
+	
+	@Test
+	public void deveVerificarListaRaiz() {
+		
+		given() // Pré Condições
+		.when()
+		.get("https://restapi.wcaquino.me/users") // Ação
+		.then() // Assertivas
+		.statusCode(200)
+		.body("$", hasSize(3))
+		.body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+		.body("age[1]", is(25))
+		.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+		.body("salary", contains(1234.5678f, 2500, null))
+		
+		;
+		
+	}
+	
+	@Test
+	public void deveFazerVerificacoesAvancadas() {
+		
+		given() // Pré Condições
+		.when()
+		.get("https://restapi.wcaquino.me/users") // Ação
+		.then() // Assertivas
+		.statusCode(200)
+		.body("$", hasSize(3))
+		.body("age.findAll{it <= 25}.size()", is(2))
+		.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+		.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+		.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+		.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+		.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+		.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+		.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+		.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+		.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+		.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+		.body("age.collect{it * 2}", hasItems(60, 50, 40 ))
+		.body("id.max()", is(3))
+		.body("salary.min()", is(1234.5678f))
+		.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+		.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)))
+		
+		
+		
+		;
+		
+	}
+	
 	
 
 }
